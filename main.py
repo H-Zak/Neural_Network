307f1ba314576a53ba6460e13064c87727eaa1fb
Zakariya HAMDOUCHI <zhamdouc@paul-f5Ar2s2.clusters.42paris.fr>
2024-09-20 18:10:42 +0200

    creation porject

--- main.py
+++ main.py
@@ -4,7 +4,14 @@ import pandas as pd
 def main():
 	print("Start")
 	data =  pd.read_csv("./data_cancer.csv")
-	x_train, x_tet, y_train, y_test = train_test_split(x,y, test_size=0.2,random=42) 
+	x = data.drop(data.columns[1], axis=1)
+	y = data.iloc[:, 1]
+	x_train, x_tet, y_train, y_test = train_test_split(x,y, test_size=0.2,random=42)
+	#start the creation of the neural
+
+	#start the training 
+
+	#test with y_test si les resultat sont bons 
 	
 
 
