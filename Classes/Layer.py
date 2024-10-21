[Bug] Using softmax function

Classes/Layer.py
@@ -1,70 +1,186 @@
 import numpy as np
-from typing import List, Callable
+from typing import Tuple, List, Callable
 from abc import ABC, abstractmethod
 
 class Layer(ABC):
-    @abstractmethod
-    def __init__(self, input_shape, number_of_neurons, layer_type : str):
-        self.weights = np.random.randn(number_of_neurons, input_shape)
+    def __init__(self, input_shape: int, number_of_neurons: int, layer_type : str):
+        self.weights = np.random.randn(number_of_neurons, input_shape) 
         self.bias = np.zeros((number_of_neurons, 1))
+        # Save activation of the previous layer
+        self.A_prev = None
+        # Save pre-activation
+        self.Z = None
 
-    def call(self, inputs):
-        Z = np.dot(self.weights, inputs) + self.bias
-        A = self.activation_ft(Z)
-        return A, Z
+        # debug
+        self.layer_type = layer_type
+
+    def forward(self, inputs: np.ndarray) -> np.ndarray:
+        self.A_prev = inputs
+        self.Z = np.dot(self.weights, inputs) + self.bias
+        A = self.activation_ft(self.Z)
+        return A
+
+    # TODO -> learning rate here? 
+    def update_parameters(self, dW: np.ndarray, db: np.ndarray, learning_rate: float):
+        self.weights -= learning_rate * dW
+        self.bias -= learning_rate * db
+
+    @abstractmethod
+    def backward(self, dA: np.ndarray) -> np.ndarray:
+        pass
     
     @abstractmethod
-    def activation_ft(self, x):
+    def activation_ft(self, x: np.ndarray) -> np.ndarray:
         pass
 
     @abstractmethod
-    def derivative_activation_ft(self, output):
+    def derivative_activation_ft(self, output: np.ndarray) -> np.ndarray:
+        pass
+
+    @abstractmethod
+    def get_gradients(self) -> Tuple[np.ndarray, np.ndarray]:
         pass
 
 class HiddenLayer(Layer):
-    def __init__(self, input_shape, number_of_neurons, layer_type : str):
+    def __init__(self, input_shape: int, number_of_neurons: int, layer_type : str):
         super().__init__(input_shape, number_of_neurons, layer_type)
-        
-    # def call(self, inputs):
-    #     Z = np.dot(self.weights, inputs) + self.bias
-    #     # print(f"------------ Zs  hidden layer ------------------------")
-    #     # print(Z)
-    #     A = self.activation_ft(Z)
-    #     # print(f"----------- Activations  Hidden Layer ----------------")
-    #     # print(A)
-    #     return A, Z
-
+    
+    def backward(self, dA: np.ndarray) -> np.ndarray:
+        # Compute dZ
+        dZ = dA * self.derivative_activation_ft(self.Z)
+        self.dW = np.dot(dZ, self.A_prev.T)
+        self.db = np.sum(dZ, axis=1, keepdims=True)
+        # Gradient of the previous layer ?  
+        dA_prev = np.dot(self.weights.T, dZ)
+        return dA_prev
+    
     # reLU function
-    def activation_ft(self, x):
+    def activation_ft(self, x: np.ndarray) -> np.ndarray:
         return np.maximum(0, x)
-
+    
     # reLU derivative
-    def derivative_activation_ft(self, x):
-        # The directive of ReLU function is 1 for x > 0 and 0 for x <= 0
-        return np.where(x > 0, 1, 0)
+    def derivative_activation_ft(self, output: np.ndarray) -> np.ndarray:
+        return np.where(output > 0, 1, 0)
+
+    def get_gradients(self) -> Tuple[np.ndarray, np.ndarray]:
+        return self.dW, self.db
+
 
 class OutputLayer(Layer):
-    def __init__(self, input_shape, number_of_neurons, layer_type : str):
+    def __init__(self, input_shape: int, number_of_neurons: int, layer_type : str):
         super().__init__(input_shape, number_of_neurons, layer_type)
 
-    # def call(self, inputs):
-    #     # print(inputs)
-    #     # print(self.weights)
-    #     Z = np.dot(self.weights, inputs) + self.bias
-    #     # print(f"------------ Zs  Output layer ------------------------")
-    #     # print(Z)
-    #     # print(Z.shape)
-    #     A = self.activation_ft(Z)
-    #     # print(f"----------- Activations  Output layer ----------------")
-    #     # print(A)
-    #     # print(A.shape)
-    #     return A, Z
+    def backward(self, dA) -> np.ndarray:
+        dZ = dA
+        self.dW = np.dot(dZ, self.A_prev.T)
+        self.db = np.sum(dZ, axis=1, keepdims=True)
+        dA_prev = np.dot(self.weights.T, dZ)
+        return dA_prev
 
-    # Sigmoid
-    def activation_ft(self, x):
-        return 1 / (1 + np.exp(-x))
+    def activation_ft(self, x: np.ndarray) -> np.ndarray:
+        exp_x = np.exp(x - np.max(x, axis=-1, keepdims=True))
+        return exp_x / np.sum(exp_x, axis=-1, keepdims=True)
 
-    def derivative_activation_ft(self, output):
-        # The derivative of sigmoid function: σ'(x) = σ(x) * (1 - σ(x))
-        sig = self.activation_ft(output)
-        return sig * (1 - sig)
+    def derivative_activation_ft(self, output: np.ndarray, y_true: np.ndarray) -> np.ndarray:
+        return output - y_true
+
+    def get_gradients(self) -> Tuple[np.ndarray, np.ndarray]:
+        return self.dW, self.db 
+
+
+# import numpy as np
+# from typing import List, Callable
+# from abc import ABC, abstractmethod
+
+# class Layer(ABC):
+#     @abstractmethod
+#     def __init__(self, input_shape, number_of_neurons, layer_type : str):
+#         self.weights = np.random.randn(number_of_neurons, input_shape)
+#         self.bias = np.zeros((number_of_neurons, 1))
+
+#     def call(self, inputs):
+#         Z = np.dot(self.weights, inputs) + self.bias
+#         A = self.activation_ft(Z)
+#         return A, Z
+
+#     @abstractmethod
+#     def backward(self, dA):
+#         # Implementar la lógica de retropropagación
+#         # Por ejemplo, calcula dZ y almacena dW y db para usar en get_gradients
+#         pass
+
+#     @abstractmethod
+#     def get_gradients(self):
+#         # Devuelve dW y db almacenados durante la retropropagación
+#         return self.dW, self.db
+
+#     def update_parameters(self, dW, db, learning_rate=0.01):
+#         self.weights -= learning_rate * dW
+#         self.bias -= learning_rate * db
+    
+#     @abstractmethod
+#     def activation_ft(self, x):
+#         pass
+
+#     @abstractmethod
+#     def derivative_activation_ft(self, output):
+#         pass
+
+# class HiddenLayer(Layer):
+#     def __init__(self, input_shape, number_of_neurons, layer_type : str):
+#         super().__init__(input_shape, number_of_neurons, layer_type)
+#         self.type_layer = "Hidden Layer"
+        
+#     # def call(self, inputs):
+#     #     Z = np.dot(self.weights, inputs) + self.bias
+#     #     # print(f"------------ Zs  hidden layer ------------------------")
+#     #     # print(Z)
+#     #     A = self.activation_ft(Z)
+#     #     # print(f"----------- Activations  Hidden Layer ----------------")
+#     #     # print(A)
+#     #     return A, Z
+
+#     # reLU function
+#     def activation_ft(self, x):
+#         return np.maximum(0, x)
+
+#     # reLU derivative
+#     def derivative_activation_ft(self, x):
+#         # The directive of ReLU function is 1 for x > 0 and 0 for x <= 0
+#         return np.where(x > 0, 1, 0)
+
+# class OutputLayer(Layer):
+#     def __init__(self, input_shape, number_of_neurons, layer_type : str):
+#         super().__init__(input_shape, number_of_neurons, layer_type)
+#         self.type_layer = "Output Layer"
+
+#     # def call(self, inputs):
+#     #     # print(inputs)
+#     #     # print(self.weights)
+#     #     Z = np.dot(self.weights, inputs) + self.bias
+#     #     # print(f"------------ Zs  Output layer ------------------------")
+#     #     # print(Z)
+#     #     # print(Z.shape)
+#     #     A = self.activation_ft(Z)
+#     #     # print(f"----------- Activations  Output layer ----------------")
+#     #     # print(A)
+#     #     # print(A.shape)
+#     #     return A, Z
+
+#     # Softmax
+#     def activation_ft(self, x):
+#         exp_x = np.exp(x - np.max(x, axis=-1, keepdims=True))
+#         return exp_x / np.sum(exp_x, axis=-1, keepdims=True)
+    
+#     def derivative_activation_ft(self, output, y_true):
+#         # y_true is a one-hot encoded vector
+#         return output - y_true
+    
+#     # # Sigmoid
+#     # def activation_ft(self, x):
+#     #     return 1 / (1 + np.exp(-x))
+
+#     # def derivative_activation_ft(self, output):
+#     #     # The derivative of sigmoid function: σ'(x) = σ(x) * (1 - σ(x))
+#     #     sig = self.activation_ft(output)
+#     #     return sig * (1 - sig)

Classes/Model.py
@@ -29,7 +29,7 @@ class Model():
         
         # Training and validation data
         self.x_train = data_train[0]
-        self.y_train = data_train[1]
+        self.y_train = self.one_hot_encoding(data_train[1])
 
         self.data_train = data_train
         self.data_valid = data_valid
@@ -45,48 +45,60 @@ class Model():
         # Keep track of loss during training and validation
         self.train_loss_history = []
         self.valid_loss_history = []
+    
+    def one_hot_encoding(self, y, num_classes=2):
+        """
+        Converts a 1D array of labels to one-hot encoded format.
+        Args:
+        y (np.ndarray): Array of labels (shape: [1, batch_size]).
+        num_classes (int): Number of output classes (for binary classification, this is 2).
+        
+        Returns:
+        np.ndarray: One-hot encoded labels (shape: [num_classes, batch_size]).
+        """
+        return np.eye(num_classes)[y.reshape(-1)].T
+   
+    def train(self):
+        inputs =  self.x_train
+        tolerance : float = 1e-8
+        for e in range(self.epochs):
+            outputs = self.network.feedforward(inputs)
+            cost = self.loss_function(self.y_train, outputs)
+            if e % 100 == 0:
+                self.train_loss_history.append(cost)
+            print(f"epoch {e+1}/{self.epochs} - loss: {cost:2.4f} - val_loss: {0}")
+            self.network.backpropagation(inputs, outputs, self.y_train, self.learning_rate)
 
     # def train(self):
-    #     inputs =  self.x_train
-    #     tolerance : float = 1e-8
+    #     inputs = self.x_train
+    #     tolerance: float = 1e-8
+    #     count_training_examples = inputs.shape[1]
     #     for e in range(self.epochs):
-    #         outputs = self.network.feedforward(inputs)
-    #         cost = self.loss_function(self.y_train, outputs)
-    #         if e % 100 == 0:
-    #             self.train_loss_history.append(cost)
-    #         print(f"epoch {e+1}/{self.epochs} - loss: {cost:2.4f} - val_loss: {0}")
-    #         self.network.backpropagation(inputs, outputs, self.y_train, self.learning_rate)
+    #         print(f"Epoch {e+1}")
+    #         indices = np.random.permutation(count_training_examples)
+    #         for start in range(0, count_training_examples, self.batch_size):
+    #             end = start + self.batch_size
+    #             batch_indices = indices[start:end]
+    #             print("Batch indices:")
+    #             print(batch_indices)
+    #             # x_batch = self.x_train[:, batch_indices]
+    #             # y_batch = self.y_train[:, batch_indices]
 
-    def train(self):
-        inputs = self.x_train
-        tolerance: float = 1e-8
-        count_training_examples = inputs.shape[1]
-        for e in range(self.epochs):
-            print(f"Epoch {e+1}")
-            indices = np.random.permutation(count_training_examples)
-            for start in range(0, count_training_examples, self.batch_size):
-                end = start + self.batch_size
-                batch_indices = indices[start:end]
-                print("Batch indices:")
-                print(batch_indices)
-                # x_batch = self.x_train[:, batch_indices]
-                # y_batch = self.y_train[:, batch_indices]
+    #             # print(x_batch)
 
-                # print(x_batch)
+    #             # print(y_batch)
 
-                # print(y_batch)
+    #             # print(x_batch)
 
-                # print(x_batch)
+    #             # print(y_batch)
 
-                # print(y_batch)
+    #             # outputs = self.network.feedforward(x_batch)
 
-                # outputs = self.network.feedforward(x_batch)
+    #             # cost = self.loss_function(y_batch, outputs)
 
-                # cost = self.loss_function(y_batch, outputs)
+    #             # self.network.backpropagation(x_batch, outputs, y_batch, self.learning_rate)
 
-                # self.network.backpropagation(x_batch, outputs, y_batch, self.learning_rate)
+    #             # if e % 100 == 0 and e == 0:
+    #             #     self.train_loss_history.append(cost)
 
-                # if e % 100 == 0 and e == 0:
-                #     self.train_loss_history.append(cost)
-
-            # print(f"epoch {e+1}/{self.epochs} - loss: {cost:2.4f} - val_loss: {0}")
+    #         # print(f"epoch {e+1}/{self.epochs} - loss: {cost:2.4f} - val_loss: {0}")

modules/loss_function.py
@@ -3,28 +3,51 @@ import numpy as np
 # Docs : If any of the predicted values in predicted_values is exactly 0 or 1, then np.log(0) will give an undefined value (-inf).
 # To avoid this problem, a small value (epsilon) is usually added to maintain numerical stability and prevent the logarithm from dealing with 0
 
-def binary_cross_entropy(predicted_values: np.ndarray, real_values: np.ndarray) -> float:
+# def binary_cross_entropy(predicted_values: np.ndarray, real_values: np.ndarray) -> float:
+#     """
+#     Compute the binary cross-entropy loss between predicted values and real values.
+
+#     Args:
+#     predicted_values (np.ndarray): Predicted probabilities (values between 0 and 1).
+#     real_values (np.ndarray): Actual binary labels (0 or 1).
+
+#     Returns:
+#     float: The binary cross-entropy loss.
+#     """
+#     # Add an optional check to make sure that the predicted_values and actual_values dimensions match.
+#     assert predicted_values.shape == real_values.shape, "Shape mismatch between predicted and real values."
+
+#     training_examples = real_values.shape[0]
+    
+#     # Prevent log(0) issues by clipping the predicted values
+#     epsilon = 1e-15
+#     predicted_values = np.clip(predicted_values, epsilon, 1 - epsilon)
+    
+#     # Compute the binary cross-entropy
+#     errors = (real_values * np.log(predicted_values)) + ((1 - real_values) * np.log(1 - predicted_values))
+#     sum_errors = (-1 / training_examples) * np.sum(errors)
+
+#     return sum_errors
+
+
+def cross_entropy_loss(y_pred: np.ndarray, y_true: np.ndarray) -> float:
     """
-    Compute the binary cross-entropy loss between predicted values and real values.
+    Compute the cross-entropy loss between predicted values and one-hot encoded labels.
 
     Args:
-    predicted_values (np.ndarray): Predicted probabilities (values between 0 and 1).
-    real_values (np.ndarray): Actual binary labels (0 or 1).
+    y_pred (np.ndarray): Predicted probabilities from softmax (shape: [num_classes, batch_size]).
+    y_true (np.ndarray): One-hot encoded actual labels (shape: [num_classes, batch_size]).
 
     Returns:
-    float: The binary cross-entropy loss.
+    float: The cross-entropy loss.
     """
-    # Add an optional check to make sure that the predicted_values and actual_values dimensions match.
-    assert predicted_values.shape == real_values.shape, "Shape mismatch between predicted and real values."
+    assert y_pred.shape == y_true.shape, "Shape mismatch between predicted and real values."
 
-    training_examples = real_values.shape[0]
-    
-    # Prevent log(0) issues by clipping the predicted values
+    # Avoid log(0) by clipping the predicted values
     epsilon = 1e-15
-    predicted_values = np.clip(predicted_values, epsilon, 1 - epsilon)
+    y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
     
-    # Compute the binary cross-entropy
-    errors = (real_values * np.log(predicted_values)) + ((1 - real_values) * np.log(1 - predicted_values))
-    sum_errors = (-1 / training_examples) * np.sum(errors)
-
-    return sum_errors
+    # Compute cross-entropy loss per example and average
+    loss_per_example = -np.sum(y_true * np.log(y_pred), axis=0)
+    loss = np.mean(loss_per_example)
+    return loss